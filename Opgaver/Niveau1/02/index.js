a1bea74749aa7c89b65504c314d90873e676ca57
orindholt <83012141+orindholt@users.noreply.github.com>
2021-08-23 09:16:24 +0200

    Add solution to assignment 1-02

---
+++ Opgaver/Niveau1/02/index.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+    function beregnForhold(a, b){
+        return a / b;
+    }
+    let resultat = beregnForhold(4, 3);
+    console.log(resultat);
+
+    function beregnForhold2(a, b){
+        if(b == 0){
+            return null;
+        } else {
+            return a / b;
+        }
+    }
+    let resultat2 = beregnForhold2(4, 0);
+    console.log(resultat2);
+});
--- Opgaver/mineReflektioner.md
+++ Opgaver/mineReflektioner.md
@@ -2,3 +2,8 @@
 
 Opgaven var nem nok og tænkte med det samme på at bruge en variabel der tælte på med et array, da man ikke måtte bruge selve farvekoden til at identificere om min if else statement var true eller false.
 
+## Niveau 1 - Opgave 02
+
+Opgaven var ikke svær at forstår og var hurtigt lavet. Ekstra opgaven var også simpel og gav mening. Jeg havde det dog svært med var at finde på reflektioner.
+
+
