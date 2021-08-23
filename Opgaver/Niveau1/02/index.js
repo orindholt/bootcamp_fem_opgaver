a976ab033d56b46dfc4cffaee8e34c192cb0f4a2
orindholt <83012141+orindholt@users.noreply.github.com>
2021-08-23 10:43:50 +0200

    Remove unnecessary else statement

--- Opgaver/Niveau1/02/index.js
+++ Opgaver/Niveau1/02/index.js
@@ -8,9 +8,8 @@ document.addEventListener("DOMContentLoaded", () => {
     function beregnForhold2(a, b){
         if(b == 0){
             return null;
-        } else {
-            return a / b;
         }
+        return a / b;
     }
     let resultat2 = beregnForhold2(4, 0);
     console.log(resultat2);
