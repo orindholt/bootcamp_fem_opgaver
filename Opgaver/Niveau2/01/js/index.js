66e70b6a4477fbb5722e3fb4514379856fffa65b
orindholt <83012141+orindholt@users.noreply.github.com>
2021-08-24 13:30:52 +0200

    Add other solution to assignment 2-01

--- Opgaver/Niveau2/01/js/index.js
+++ Opgaver/Niveau2/01/js/index.js
@@ -1,13 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const buttons = document.querySelectorAll("#buttons button");
-
-    buttons.forEach(btnElement =>{
-        btnElement.addEventListener("click", (event)=>{
-            for(let i = 0; i<buttons.length; i++){
-                buttons[i].style.backgroundColor = "#eee";
-            }
+    const buttonContainer = document.querySelector("#buttons");
+    const buttons = document.querySelectorAll("button");
+    
+    buttonContainer.addEventListener("click", (event)=>{
+        if(event.target.localName == "button"){
+            buttons.forEach(btn => btn.removeAttribute("style"));
             event.target.style.backgroundColor = "#7f7";
-        });
-
+        }
     });
 });
